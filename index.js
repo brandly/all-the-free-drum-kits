fd295c909c9147afa4288c5c803dfd3208387db1
brandly <brandly13@gmail.com>
2015-02-04 16:46:21 -0500

    no longer recurses forever

--- index.js
+++ index.js
@@ -5,39 +5,43 @@ var trumpet = require('trumpet');
 var baseUrl = 'http://www.freedrumkits.net';
 var kitsPerPage = 10;
 
-function last (list) {
-  if (list.length) {
-    return list[list.length - 1];
-  }
-}
-
-function createDrumKitDownloader () {
+function createDrumKitFinder () {
   var tr = trumpet();
 
   tr.selectAll('.rd_file .folder_url', function (anchor) {
-    var href = anchor.getAttribute('href');
-    var drumKitName = last(href.split('/'));
+    tr.emit('drumKit', anchor.getAttribute('href'));
+  });
 
+  return tr;
+}
+
+function downloadDrumKit (href) {
+    var drumKitName = href.split('/').pop();
     console.log('Downloading ' + drumKitName);
 
     var out = fs.createWriteStream(drumKitName + '.zip');
     request(baseUrl + href + '/download')
       .pipe(out);
-  });
-
-  return tr;
 }
 
 // Each URL is like `?start=10` to skip the first 10 kits.
 function getDrumKits (start) {
   var pageRequest = request(baseUrl + '/drum-kits?start=' + start)
 
-  pageRequest.on('end', function () {
-    // Get the next page!
-    getDrumKits(start + 10);
-  });
+  var pageHasDrumKit = false;
+  pageRequest
+    .pipe(createDrumKitFinder())
+    .on('drumKit', function (href) {
+      pageHasDrumKit = true;
+      downloadDrumKit(href);
+    });
 
-  pageRequest.pipe(createDrumKitDownloader());
+  pageRequest.on('end', function () {
+    if (pageHasDrumKit) {
+      // Get the next page!
+      getDrumKits(start + kitsPerPage);
+    }
+  });
 }
 
 // lezdoit
